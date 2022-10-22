c0ea9bb7e79eef0cb4f8a893d59bef4890df4ed5
Иван Романовский <i.v.romanovskiy@students.psu.by>
2022-10-22 13:49:53 +0300

    Add and set up classes for working with EF

---
+++ InnoGotchi.Persistence/EntityTypeConfigurations/UserConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using InnoGotchi.Domain;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InnoGotchi.Persistence.EntityTypeConfigurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(user => user.Id);
+            builder.HasIndex(user => user.Id).IsUnique();
+            builder.Property(user => user.FirstName).HasMaxLength(30);
+            builder.Property(user => user.LastName).HasMaxLength(30);
+        }
+    }
+}
---
+++ InnoGotchi.Persistence/InnoGotchiDbContext.cs
@@ -0,0 +1,22 @@
+﻿using InnoGotchi.Application.Interfaces;
+using InnoGotchi.Domain;
+using InnoGotchi.Persistence.EntityTypeConfigurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace InnoGotchi.Persistence
+{
+    public class InnoGotchiDbContext : DbContext, IUsersDbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public InnoGotchiDbContext(DbContextOptions<InnoGotchiDbContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new UserConfiguration());
+            base.OnModelCreating(builder);
+        }
+
+    }
+}
