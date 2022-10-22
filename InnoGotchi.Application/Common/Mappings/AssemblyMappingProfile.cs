b4bc19a2243e959fd74be44bc1e0fcda2c9ef4f2
Иван Романовский <i.v.romanovskiy@students.psu.by>
2022-10-22 14:48:47 +0300

    update AssemblyMappingProfile.cs

--- InnoGotchi.Application/Common/Mappings/AssemblyMappingProfile.cs
+++ InnoGotchi.Application/Common/Mappings/AssemblyMappingProfile.cs
@@ -1,13 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using System.Reflection;
 
 namespace InnoGotchi.Application.Common.Mappings
 {
-    public class AssemblyMappingProfile
+    public class AssemblyMappingProfile : Profile
     {
+        public AssemblyMappingProfile(Assembly assembly) =>
+            ApplyMappingsFromAssembly(assembly);
 
+        private void ApplyMappingsFromAssembly(Assembly assembly)
+        {
+            var types  = assembly.GetExportedTypes()
+                .Where(type => type.GetInterfaces()
+                    .Any(type => type.IsGenericType &&
+                    type.GetGenericTypeDefinition() == typeof(IMapWith<>)))
+                .ToList();
+            foreach (var type in types)
+            {
+                var instance = Activator.CreateInstance(type);
+                var methodInfo = type.GetMethod("Mapping");
+                methodInfo?.Invoke(instance, new object[] { this });
+            }
+        }
     }
 }
