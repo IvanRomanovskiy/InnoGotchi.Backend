4fcb890e7dde59c9a4fef1db2e78cbb3a0cfee43
Иван Романовский <i.v.romanovskiy@students.psu.by>
2022-10-22 14:04:09 +0300

    Add and set up classes for working with AutoMapper

---
+++ InnoGotchi.Application/Common/Mappings/AssemblyMappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoGotchi.Application.Common.Mappings
+{
+    public class AssemblyMappingProfile
+    {
+
+    }
+}
